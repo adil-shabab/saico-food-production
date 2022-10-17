b0019c52e4c3c0e1741d1de83a89ffe627bab803
Adil Shabab <adilshabab2004@gmail.com>
2022-10-17 16:12:21 +0530

    updating second slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
-$('.owl-carousel').owlCarousel({
+$('.category-carousel').owlCarousel({
     loop:true,
-    margin:10,
+    margin:0,
     nav:true,
+    dots:false,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
             items:3
