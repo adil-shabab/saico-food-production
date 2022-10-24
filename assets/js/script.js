535b6103b14549a60b5b06055e03589035ee7abb
Adil Shabab <adilshabab2004@gmail.com>
2022-10-24 15:54:10 +0530

    about page completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,4 +55,43 @@ $('.testimonialCarousel').slick({
 
 
 
+
+
+
+
+//testimonial slider
+$('.testimonialCarousel2').slick({
+    infinite: true,
+    slidesToShow: 2,
+    margin: 10,
+    slidesToScroll: 1,
+    // autoplay: true,
+    dots: false,
+    arrows: true,
+    responsive: [{
+            breakpoint: 1099,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+    ]
+});
+
+
+
+
 // button js 
