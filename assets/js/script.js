b21f2c6eea159f39b036dfbac7958439627def92
Adil Shabab <adilshabab2004@gmail.com>
2022-10-24 17:10:00 +0530

    contact section started

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,4 +94,16 @@ $('.testimonialCarousel2').slick({
 
 
 
-// button js 
+// input js 
+// JavaScript for label effects only
+$(window).load(function(){
+    $(".input-field input").val("");
+    
+    $(".input-effect input").focusout(function(){
+        if($(this).val() != ""){
+            $(this).addClass("has-content");
+        }else{
+            $(this).removeClass("has-content");
+        }
+    })
+});
