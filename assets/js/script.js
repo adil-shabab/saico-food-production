0b006cd24e8c4067402207d35cd2ee36f58a55a4
Adil Shabab <adilshabab2004@gmail.com>
2022-10-25 12:38:29 +0530

    product page pending jumping to home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,3 @@
-// category slider 
-$('.category-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    nav:true,
-    dots:false,
-    navText: ["<i class='arrow fa-solid fa-chevron-left'></i>","<i class='arrow fa-solid fa-chevron-right'></i>"],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
-    }
-})
 
 
 
