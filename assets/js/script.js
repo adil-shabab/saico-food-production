9069420d7b2f92849bfad9fcd143f538f711f051
Adil Shabab <adilshabab2004@gmail.com>
2022-10-25 19:24:28 +0530

    some more cahgnes added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 // category slider 
-// $('.category-carousel').owlCarousel({
-//     loop:true,
-//     margin:0,
-//     nav:true,
-//     dots:false,
-//     navText: ["<i class='arrow fa-solid fa-chevron-left'></i>","<i class='arrow fa-solid fa-chevron-right'></i>"],
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:2
-//         },
-//         1000:{
-//             items:3
-//         }
-//     }
-// })
+$('.category-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    dots:false,
+    navText: ["<i class='arrow fa-solid fa-chevron-left'></i>","<i class='arrow fa-solid fa-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
 
 
 
