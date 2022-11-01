f17c819e3b34ccdc4c6f007657d67dfb0d2a2d29
Adil Shabab <adilshabab2004@gmail.com>
2022-11-01 11:43:23 +0530

    some more changes added

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,26 +1,8 @@
-// input js 
-// JavaScript for label effects only
-// $(window).load(function(){
-//     $(".input-field input").val("");
-    
-//     $(".input-effect input").focusout(function(){
-//         if($(this).val() != ""){
-//             $(this).addClass("has-content");
-//         }else{
-//             $(this).removeClass("has-content");
-//         }
-//     })
-// });
-
 
 name_box = document.querySelector('.name-input')
 email_box = document.querySelector('.email-input')
 message_box = document.querySelector('.message-input')
 
-
-console.log(name_box)
-console.log(email_box)
-console.log(message_box)
 
 
 name_input = name_box.getElementsByTagName('input')
@@ -32,15 +14,13 @@ email_label = email_box.getElementsByTagName('label')
 message_input = message_box.getElementsByTagName('input')
 message_label = message_box.getElementsByTagName('label')
 
-
-console.log(name_input)
 name_input[0].addEventListener('change', function(){
     if (name_input.value != ""){
         name_label[0].style.display = "none"
     }
 })
 
-console.log(email_input)
+
 email_input[0].addEventListener('change', function(){
     if (email_input.value != ""){
         email_label[0].style.display = "none"
@@ -48,7 +28,6 @@ email_input[0].addEventListener('change', function(){
 })
 
 
-console.log(message_input)
 message_input[0].addEventListener('change', function(){
     if (message_input.value != ""){
         message_label[0].style.display = "none"
--- assets/js/script.js
+++ assets/js/script.js
@@ -123,16 +123,3 @@ $(document).on('click', '.owl-item>div', function() {
 });
 
 
-// input js 
-// JavaScript for label effects only
-$(window).load(function(){
-    $(".input-field input").val("");
-    
-    $(".input-effect input").focusout(function(){
-        if($(this).val() != ""){
-            $(this).addClass("has-content");
-        }else{
-            $(this).removeClass("has-content");
-        }
-    })
-});
